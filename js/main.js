15e219112a0cb7ba0c0730699a75a493833d34eb
Khashayar Nowruzi <khrnowruzi@gmail.com>
2022-11-16 11:14:37 +0330

    uplading

---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    var firstListOfContent = $('#firstListOfContent');
+    var countFirstListOfContent = $('#firstListOfContent > li').length * 20;
+    var tempCountFirstListOfContent = 0;
+    var clockFirstListOfContent;
+
+    beginfirstListOfContent();
+
+    $('#firstListOfContent > li').hover(function () {
+        clearInterval(clockFirstListOfContent);
+    }, function () {
+        beginfirstListOfContent();
+    });
+
+    function beginfirstListOfContent() {
+        clockFirstListOfContent = setInterval(function () {
+            tempCountFirstListOfContent += 20;
+            if (tempCountFirstListOfContent == countFirstListOfContent)
+                tempCountFirstListOfContent = 0;
+            firstListOfContent.animate({ top: -tempCountFirstListOfContent }, 1000);
+        }, 3000);
+    };
+    $("[data-toggle='tooltip']").tooltip({ animate: true });
+});
